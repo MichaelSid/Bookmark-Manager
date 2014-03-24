2caac2cfc0914f1ec97dceb3dd90250c20199dce
Michael <michael.sidon@gmail.com>
2014-03-24 17:02:41 +0000

    Added database cleaner for tests

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ source 'https://www.rubygems.org/'
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'database_cleaner'
+
 
 group :development, :test do 
 	gem 'rspec'
--- server.rb
+++ server.rb
@@ -5,6 +5,8 @@ get '/' do
 	erb :index
 end
 
+#we are checking what environment we're in, defaulting to development.
+
 env = ENV["RACK_ENV"] || "development"
 #we're telling datamapper to use a postgres database on localhost. The name will be
 #bookmark_manager_test" or "bookmark_manager_development" depending on the environment.
